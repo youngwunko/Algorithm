b39901b14d3fcd508607ca6f22db438fbfb1b387
youngwunko <hiaro711@gmail.com>
2021-09-07 16:20:51 +0900

    solve 11650

---
+++ BOJ/10000~/11650.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(pair<int, int> A, pair<int, int> B) {
+	if (A.first < B.first) {
+		return true;
+	}
+	else if (A.first == B.first) {
+		if (A.second < B.second)
+			return true;
+	}
+	return false;
+}
+
+int main(void) {
+	int N; cin >> N;
+	vector<pair<int, int> > pos;
+	int x, y;
+
+	while (N--) {
+		cin >> x >> y;
+		pos.push_back({ x, y });
+	}
+	sort(pos.begin(), pos.end(), comp);
+
+	for (pair<int, int> p : pos) {
+		cout << p.first << " " << p.second << "\n";
+	}
+	cout << "\n";
+
+	return 0;
+}
